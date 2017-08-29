b731b93d290b00dd79e9a93160d031d76e260bab
jack <1395093509@qq.com>
2017-08-29 09:42:27 +0800

    名称修改

---
+++ vui/Abc/src/Index.vue
@@ -0,0 +1,82 @@
+<template>
+
+</template>
+
+<script>
+/**
+ * Abc 名称
+ * @module packages/Abc
+ * @desc 描述
+ * @rules
+ *   - 使用规则
+ * @param {string} [type] - 显示类型
+ *
+ * @example
+ * <Abc>xxx</Abc>
+ */
+import PropTypes from 'vue-types'
+export default {
+  name: 'Abc',
+
+  props: {
+    prefixCls: PropTypes.string.def('abc'),
+    text: PropTypes.oneOfType([
+      String,
+      Number,
+    ]),
+    max: PropTypes.number.def(99),
+    dot: Boolean,
+    hidden: Boolean,
+    color: String,
+    textColor: String,
+    size: PropTypes.oneOf([
+      'xs',
+      'sm',
+      'md',
+      'lg',
+      'xl',
+    ]),
+    shape: PropTypes.oneOf([
+      'dot',
+      'circle',
+      'radius',
+      'square',
+    ]).def('circle'),
+  },
+
+  computed: {
+    classes() {
+      const {
+        prefixCls,
+        type,
+        size,
+        shape,
+        block,
+        hollow,
+        disabled,
+      } = this.$props
+
+      return {
+        [`${prefixCls}`]: true,
+        [`${prefixCls}-${type}`]: type,
+        [`${prefixCls}-${size}`]: size,
+        [`is-${shape}`]: shape,
+        'is-hollow': hollow,
+        'is-block': block,
+        'disabled': disabled,
+      }
+    },
+    // styles() {
+    //   return [
+    //     this.color ? {color: this.color} : {},
+    //     this.bg ? {backgroundColor: this.bg} : {},
+    //   ]
+    // },
+  },
+}
+</script>
+
+<style lang="stylus">
+@import "./style";
+
+</style>
---
+++ vui/Flex/src/Col.vue
@@ -0,0 +1,123 @@
+
+<script>
+/**
+ * Col 栅格列
+ * @module packages/Col
+ * @desc 24 栅格系统。
+ * @rules
+ *   - 只有`col`可以作为`row`的直接元素
+ *   - 如果一个`row`中的`col`总和超过 24，那么多余的`col`会作为一个整体另起一行排列
+ *   - xs <768px
+ *   - sm ≥768px
+ *   - md ≥992px
+ *   - lg ≥1200px
+ *   - xl ≥1600px
+ * @param {number} [span] - 栅格占位格数，为 0 时相当于 `display: none`
+ * @param {number} [order] - 栅格顺序，`flex` 布局模式下有效
+ * @param {number} [offset] - 栅格左侧的间隔格数，间隔内不可以有栅格
+ * @param {number} [push] - 栅格向右移动格数
+ * @param {number} [pull] - 栅格向左移动格数
+ *
+ * @example
+ * <Row >
+ *   <Col>...</Col>
+ * </Row>
+ */
+import PropTypes from 'vue-types'
+export default {
+  name: 'Col',
+
+  props: {
+    prefixCls: PropTypes.string.def('col'),
+    tag: PropTypes.string.def('div'),
+    span: PropTypes.number.def(24),
+    order: Number,
+    offset: Number,
+    push: Number,
+    pull: Number,
+    xs: [Number, Object],
+    sm: [Number, Object],
+    md: [Number, Object],
+    lg: [Number, Object],
+    xl: [Number, Object],
+  },
+
+  computed: {
+    gutter() {
+      let parent = this.$parent
+      while (parent && parent.$options.componentName !== 'Row') {
+        parent = parent.$parent
+      }
+      return parent ? parent.gutter : 0
+    },
+    classes() {
+      const $props = this.$props
+      const {
+        prefixCls,
+        span,
+        order,
+        offset,
+        push,
+        pull,
+      } = this.$props
+
+      let sizeClassObj = {}
+
+      const sizes = ['xs', 'sm', 'md', 'lg', 'xl']
+      sizes.forEach(size => {
+        let sizeProps = {}
+        if (typeof $props[size] === 'number') {
+          sizeProps.span = $props[size]
+        } else if (typeof $props[size] === 'object') {
+          sizeProps = $props[size] || {}
+        }
+
+        sizeClassObj = {
+          ...sizeClassObj,
+          [`${prefixCls}-${size}-${sizeProps.span}`]: sizeProps.span !== undefined,
+          [`${prefixCls}-${size}-order-${sizeProps.order}`]: sizeProps.order || sizeProps.order === 0,
+          [`${prefixCls}-${size}-offset-${sizeProps.offset}`]: sizeProps.offset || sizeProps.offset === 0,
+          [`${prefixCls}-${size}-push-${sizeProps.push}`]: sizeProps.push || sizeProps.push === 0,
+          [`${prefixCls}-${size}-pull-${sizeProps.pull}`]: sizeProps.pull || sizeProps.pull === 0,
+        }
+      })
+
+      return {
+        [`${prefixCls}-${span}`]: span !== undefined,
+        [`${prefixCls}-order-${order}`]: order,
+        [`${prefixCls}-offset-${offset}`]: offset,
+        [`${prefixCls}-push-${push}`]: push,
+        [`${prefixCls}-pull-${pull}`]: pull,
+        ...sizeClassObj,
+      }
+    },
+    styles() {
+      const {
+        gutter,
+      } = this
+      const style = {}
+      if (gutter > 0) {
+        style.paddingLeft = `${gutter / 2}px`
+        style.paddingRight = style.paddingLeft
+      }
+      return style
+    },
+  },
+
+  render(h) {
+    const {
+      tag,
+    } = this.$props
+    const { classes, styles } = this
+
+    return h(tag, {
+      class: classes,
+      style: styles,
+    }, this.$slots.default)
+  },
+}
+</script>
+
+<style lang="stylus">
+
+</style>
